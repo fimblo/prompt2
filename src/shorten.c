5d7e6fea099b41e0b6e2a37fa35b8364388b8238
Mattias Jansson <fimblo@yanson.org>
2024-01-05 21:37:47 +0100

    style: comments and whitespace

--- src/shorten.c
+++ src/shorten.c
@@ -16,41 +16,60 @@
 */
 
 void truncatePath(char *originalPath, int maxWidth) {
+
+  // Sanity checks
   int originalPathLen = strlen(originalPath);
-  if (originalPathLen <= maxWidth) {
+  if (originalPathLen <= maxWidth) return;
+  if (maxWidth < 3) {
+    for (int i = 0; i < maxWidth; i++) originalPath[i] = '.';
+    originalPath[3] = '\0';
     return;
   }
 
+  // How much to shrink?
   int shrinkage = originalPathLen - maxWidth;
   if (shrinkage <= 3) {
     shrinkage = 3; // for the ellipsis
   }
   else {
     shrinkage += 3;
-}
+  }
+
+  // Shrink it
+  char rebuildPath[originalPathLen + 1];
+  strcpy(rebuildPath, "...");
+  strncat(rebuildPath,
+          originalPath + shrinkage,     // forward shrinkage chars
+          originalPathLen - shrinkage); // copy this many chars
 
 
-  char rebuildPath[originalPathLen + 1]; // +1 for the null terminator
-  strcpy(rebuildPath, "..."); // Start with an ellipsis
-
-  strncat(rebuildPath, originalPath + shrinkage, originalPathLen - shrinkage);
-
-  // Copy the result back to originalPath
   strcpy(originalPath, rebuildPath);
 }
 
 
 
+
+
 void accordionPath(char *originalPath, int maxWidth) {
+
+  // Sanity checks
   int originalPathLen = strlen(originalPath);
-  if (originalPathLen <= maxWidth) {
+  if (originalPathLen <= maxWidth) return;
+  if (maxWidth < 3) {
+    for (int i = 0; i < maxWidth; i++) originalPath[i] = '.';
+    originalPath[3] = '\0';
     return;
   }
 
+  
+  // tmp vars as my workbench, these will be messed with
   char tmpPath[originalPathLen];
-  char rebuildPath[originalPathLen]; // for storing the final path
+  int  tmpPathLength = originalPathLen;
+
+  // As I step through the directories, I place dirs (short or long)
+  // here after I decide what to do.
+  char rebuildPath[originalPathLen];
   memset(rebuildPath, '\0', originalPathLen);
-  int tmpPathLength = originalPathLen;
 
   // store the first char if it's a special char.
   if (originalPath[0] == '~' || originalPath[0] == '+') {
@@ -60,15 +79,16 @@ void accordionPath(char *originalPath, int maxWidth) {
     strcpy(tmpPath, originalPath);
   }
 
-  char lastToken[originalPathLen];  // for the dir the user is standing in
-  int shortDirWasUsed;
+  // use tokenizer to walk through the dir levels
+  char  lastToken[originalPathLen];  // for the dir the user is standing in, we want to preserve that
+  int   shortDirWasUsed;
   char *token = strtok(tmpPath, "/"); 
   while (token != NULL) { 
-    int shortDirLength = 3; // +1 for slash, +1 for nullchar
+    int shortDirLength = 3; // +1 for slash, +1 for terminator
     char shortDir[shortDirLength];
     sprintf(shortDir, "/%c", token[0]);
 
-    int longDirLength = strlen(token) + 2; // +1 for slash, +1 for nu..char
+    int longDirLength = strlen(token) + 2; // +1 for slash, +1 for terminator ill be back
     char longDir[longDirLength];
     snprintf(longDir, (size_t) longDirLength, "/%s", token);
 
@@ -87,6 +107,10 @@ void accordionPath(char *originalPath, int maxWidth) {
     token = strtok(NULL, "/"); 
   }
 
+   /*
+    If the last directory added to rebuildPath was a shortDir, replace
+    it with the full directory name, since I want that behaviour
+   */
   if (shortDirWasUsed) {
     char *lastSlash = strrchr(rebuildPath, '/');
     if (lastSlash != NULL) {
