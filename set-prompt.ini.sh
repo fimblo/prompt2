34c076f3f581f810409b7f9c1f8e11858df9a366
Mattias Jansson <fimblo@yanson.org>
2024-01-27 20:48:26 +0100

    feat: update bash prompt setter script

--- set-prompt.ini.sh
+++ set-prompt.ini.sh
@@ -1,9 +1,13 @@
 # Source me!
+REPO_DIR=$(dirname ${BASH_SOURCE[0]})
+PROMPT_BIN=$(realpath $REPO_DIR/bin/prompt)
+unset REPO_DIR
+
 export GP2_NON_GIT_PROMPT="${TITLEBAR}\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\] $ "
 export GP2_GIT_PROMPT="<@{Repo.name}><@{Repo.branch.name}> b/a(@{Diverge.behind},@{Diverge.ahead}) s(@{Staged.num}) u(@{Unstaged.num}) @{CWD.home_path}\n$ ";
 
 prompt_cmd() {
-  PS1="$(bin/prompt)"
+  PS1="$($PROMPT_BIN)"
 }
 
 PROMPT_COMMAND=prompt_cmd
