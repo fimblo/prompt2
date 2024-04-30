979b1d93471bffd5fee068d5ee65015626fa439d
Mattias Jansson <fimblo@yanson.org>
2024-04-30 21:58:53 +0200

    feat: move config file..
    and adapt set-prompt.ini.sh to pass the config file as arg1 to prompt2.

--- config/set-prompt.ini.sh
+++ config/set-prompt.ini.sh
@@ -3,13 +3,16 @@
 
 # Get the full path to the prompt2 binary
 CONFIG_DIR=$(dirname ${BASH_SOURCE[0]})
-PROMPT_BIN=$(realpath $CONFIG_DIR/../bin/prompt2)
-unset CONFIG_DIR
+PROMPT2_BIN=$(realpath $CONFIG_DIR/../bin/prompt2)
+PROMPT2_CONFIG=$(realpath "$CONFIG_DIR/dot.prompt2_config.ini")
+
 
 # Simplest shell function to set PS1 to the output of prompt2
 prompt_cmd() {
-  PS1="$($PROMPT_BIN)"
+  PS1="$($PROMPT2_BIN $PROMPT2_CONFIG)"
 }
 
 # Make this function run every time I hit enter
 PROMPT_COMMAND=prompt_cmd
+
+unset CONFIG_DIR
