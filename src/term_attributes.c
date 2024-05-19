4eb2fbf0c76a288ecadfb1bac2042ed29a492224
Mattias Jansson <fimblo@yanson.org>
2024-05-19 15:13:46 +0200

    feat: stand-alone escape-code generator

--- src/term_attributes.c
+++ src/term_attributes.c
@@ -1547,31 +1547,3 @@ const char *replace_attribute_tokens(const char *string, dictionary *attribute_d
     *result_ptr = '\0'; // Null-terminate the result string
     return result;
 }
-
-
-// int main(int argc, char *argv[]) {
-//   if (argc < 2) {
-//     printf("Usage:\n");
-//     printf(" get-terminal-escape-sequence <plaintext style and colour>\n");
-//     printf("\n");
-//     printf("Example: to get the terminal escape code '\\[\\e[1;34;47m\\]', run:\n");
-//     printf(" get-terminal-escape-sequence 'bold;fg blue;bg white'\n");
-//     exit(1);
-//   }
-//   //  printf("arg: %s\n", argv[1]);
-//   
-//   dictionary *attribute_dict = create_attribute_dict();
-// 
-//   int exit_status=-1;
-//   const char *seq = get_attribute_combo(attribute_dict, argv[1]);
-//   if (seq) {
-//     printf("%s\n", seq);
-//     exit_status=0;
-//   }
-// 
-// 
-//   // Free the dictionary
-//   free_attribute_dict(attribute_dict);
-//   exit(exit_status);
-// }
-// 
---
+++ src/termstylegen.c
@@ -0,0 +1,42 @@
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef __unix__
+#include <iniparser/dictionary.h>
+#elif __APPLE__
+#include <dictionary.h>
+#else
+#error "Unknown or unsupported OS"
+#endif
+#include "prompt2-utils.h"
+
+#include "term_attributes.h"
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage:\n");
+    printf(" get-terminal-escape-sequence <plaintext style and colour>\n");
+    printf("\n");
+    printf("Example: to get the terminal escape code '\\[\\e[1;34;47m\\]', run:\n");
+    printf(" get-terminal-escape-sequence 'bold;fg blue;bg white'\n");
+    exit(1);
+  }
+  //  printf("arg: %s\n", argv[1]);
+  
+  dictionary *attribute_dict = create_attribute_dict();
+
+  int exit_status=-1;
+  const char *seq = get_attribute_combo(attribute_dict, argv[1]);
+  if (seq) {
+    printf("%s\n", seq);
+    exit_status=0;
+  }
+
+
+  // Free the dictionary
+  free_attribute_dict(attribute_dict);
+  exit(exit_status);
+}
+
